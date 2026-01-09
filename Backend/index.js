3ed0d27c2605d231d515f7b0dfb1e0ab40230177
Om Gupta <omgupta@Oms-MacBook-Air-2.local>
2026-01-09 21:48:49 +0530

    add vercel link

--- Backend/index.js
+++ Backend/index.js
@@ -21,10 +21,15 @@ const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+
 const corsOptions={
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "job-portal-frontend-six-alpha.vercel.app"
+  ],
   credentials:true
 }
+
 app.use(cors(corsOptions))
 
 const PORT= process.env.PORT || 3000
